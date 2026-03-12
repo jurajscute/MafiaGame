a5e59df37eec13985df00382dcdad31b5075238f
jurajscute <newchaosgod@gmail.com>
2026-03-12 22:15:04 +0100

    Fix

--- js/roles.js
+++ js/roles.js
@@ -3,53 +3,31 @@ export const roles = {
 villager:{
 team:"village",
 nightAction:null
+description:"You have no special ability. Find the mafia."
 },
 
 mafia:{
 team:"mafia",
 nightAction:"kill"
+description:"Work with the mafia to eliminate villagers each night."
 },
 
 doctor:{
 team:"village",
 nightAction:"save"
+description:"Choose one player each night to protect from being killed."
 },
 
 sheriff:{
 team:"village",
 nightAction:"investigate"
+description:"Investigate a player each night to learn if they are suspicious."
 },
 
 jester:{
 team:"neutral",
 nightAction:null
-}
-
-}
-
-export const roles = {
-
-mafia:{
-nightAction:"kill",
-description:"Work with the mafia to eliminate villagers each night."
-},
-
-doctor:{
-nightAction:"save",
-description:"Choose one player each night to protect from being killed."
-},
-
-sheriff:{
-nightAction:"investigate",
-description:"Investigate a player each night to learn if they are suspicious."
-},
-
-jester:{
 description:"You win if the town votes you out."
-},
-
-villager:{
-description:"You have no special ability. Find the mafia."
 }
 
 }
