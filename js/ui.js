15ad9b3030fa07871ea31930fb3979ba100290a0
jurajscute <newchaosgod@gmail.com>
2026-03-16 13:40:17 +0100

    Workplease

--- js/ui.js
+++ js/ui.js
@@ -11,19 +11,14 @@ return `<button onclick="${action}">${text}</button>`
 }
 
 export function passPhone(name, action){
+  render(`
+    <div class="pass-screen">
+      <p class="pass-label">PASS THE PHONE</p>
 
-render(`
-
-<p class="pass-label">PASS THE PHONE</p>
-
-<div class="card pass-card">
-
-<h1 class="pass-player"><strong>${name}</strong></h1>
-
-<button onclick="${action}">I'm Ready</button>
-
-</div>
-
-`)
-
+      <div class="card pass-card">
+        <h1 class="pass-player"><strong>${name}</strong></h1>
+        <button onclick="${action}">I'm Ready</button>
+      </div>
+    </div>
+  `)
 }
