15dea21bae1f62072a556981e0c8bd914beb0553
jurajscute <newchaosgod@gmail.com>
2026-03-16 23:52:05 +0100

    Polish!

--- js/ui.js
+++ js/ui.js
@@ -12,13 +12,38 @@ return `<button onclick="${action}">${text}</button>`
 
 export function passPhone(name, action){
   render(`
-    <div class="pass-screen">
-      <p class="pass-label">PASS THE PHONE</p>
 
-      <div class="card pass-card">
-        <h1 class="pass-player"><strong>${name}</strong></h1>
-        <button onclick="${action}">I'm Ready</button>
+    <div class="pass-screen">
+
+      <div class="card role-pass-card pass-phone-card">
+
+        <div class="role-pass-hero">
+          <div class="role-pass-kicker">Pass the Phone</div>
+
+          <h2 class="role-pass-title">
+            ${name}
+          </h2>
+
+          <div class="role-pass-subtitle">
+           Greetings ${name}, make sure no one can see your screen.
+           </div>
+        </div>
+
+        <div class="reveal-role-description-wrap">
+          <p class="role-description reveal-role-description">
+            When you're ready, continue privately.
+          </p>
+        </div>
+
+        <div class="role-pass-actions">
+          <button class="primary-btn" onclick="${action}">
+            I'm Ready
+          </button>
+        </div>
+
       </div>
+
     </div>
+
   `)
 }
